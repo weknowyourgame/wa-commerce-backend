6fbcab9a9d3af2facb91f746f0bf44dab8277bec
Sarthak2 <aniki123.ai@gmail.com>
2025-07-27 21:18:40 +0530

    hono --init

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,39 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { logger as honoLogger } from "hono/logger";
 
-export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+// Hono app
+const app = new Hono();
+
+// Middleware
+app.use("*", honoLogger());
+app.use(
+  "*",
+  cors({
+    origin: "*",
+    allowMethods: ["POST", "OPTIONS", "GET"],
+    allowHeaders: ["Content-Type", "Authorization"],
+    maxAge: 86400,
+  })
+);
+
+app.get("/", (c) => {
+  return c.json({
+    name: "Whatsapp Worker",
+    version: "1.0.0",
+    endpoints: [""],
+    authentication: "No authentication required (for now)",
+  });
+});
+
+// Fetch Endpoints
+
+// Health Check Endpoint
+app.get("/health", (c) => {
+  return c.json({
+    status: "healthy",
+    timestamp: new Date().toISOString(),
+  });
+});
+
+export default app;
